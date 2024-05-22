5854e9124d1e161d9d7dba6c4893f6d9d856f9ce
to-rex <torex.amaki@gmail.com>
2024-05-22 21:42:55 +0500

    first

---
+++ main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+
+from controllers.teas import TEA
+from models.response import ResponseData, RequestData
+
+app = FastAPI()
+
+
+@app.get('/')
+def index():
+    return 'Hello, World!'
+
+
+@app.post('/api/encrypt', response_model=ResponseData)
+def encrypt(request_data: RequestData):
+    try:
+        key = request_data.key
+        data = request_data.data
+        encrypted_data = TEA.encrypt_tea(data, key)
+        return ResponseData(status=200, message="Encoding successful", data=encrypted_data)
+
+    except Exception as e:
+        raise ResponseData(status=500, message="Encoding failed", data=str(e))
+
+
+@app.post('/api/decrypt', response_model=ResponseData)
+def decrypt(request_data: RequestData):
+    try:
+        key = request_data.key
+        data = request_data.data
+        decrypted_data = TEA.decrypt_tea(data, key)
+        return ResponseData(status=200, message="Decoding successful", data=decrypted_data)
+
+    except Exception as e:
+        raise ResponseData(status=500, message="Decoding failed", data=str(e))
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app)
